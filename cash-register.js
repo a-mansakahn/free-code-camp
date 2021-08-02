6277eb83e0875c45d09cabd43e2ac615a8c77f02
Alvar Mansakahn <a.mansakahn@protonmail.com>
2021-08-02 13:25:20 -0400

    Final. Passed all its tests

--- cash-register.js
+++ cash-register.js
@@ -14,35 +14,51 @@ function checkCashRegister(price, cash, cid) {
   };
   //variables
   let changeSum = cash*100 - price*100;
-  let drawerTotal;
+  let totalChange = changeSum;
+  let drawerTotal = 0; //the total amount of money in the drawer as a whole number
   let status = "";
   let change = [];
 
   //get drawerTotal
   for(let x=0; x<cid.length; x++){
-    drawerTotal += (cid[x][1]*100);
+    drawerTotal += cid[x][1]*100;
   }
+
   //First check
   if(changeSum > drawerTotal){
-    status = "INSUFFICIENT_FUNDS";
-    change = [];
+    return {"status": "INSUFFICIENT_FUNDS", "change": []};
   }
-  else if(changeSum == drawerTotal){
-    status = "CLOSED";
-    change = cid;
-  }
-  else if(changeSum < drawerTotal){
-    let newCid = cid.filter(elem => elem[1] !== 0).reverse();
-    let currency = elem[0];
-    for(let x=0; x < newCid.length; x++){
-      //go to the first one, take money out as needed, go to the next one when empty
-      while(changeSum >= newCid[x][1] && changeSum > 0){
 
-      }
+  let newCid = cid.filter(elem => elem[1] !== 0).reverse();
+  //console.log(newCid)
+  for(let x=0; x < newCid.length; x++){
+    //go to the first one, take money out as needed, go to the next one when empty
+    let currency = newCid[x][0];
+    let currAmount = newCid[x][1]*100;
+    let amount = 0;
+    //console.log(currency, currAmount, amount, changeSum)
+    //Whats the difference between CURRENCY_UNIT[currency] and newCid[x][1]?
+    while(changeSum >= CURRENCY_UNIT[currency] && currAmount > 0){
+      //changed changeSum > 0 to currAmount > 0
+      amount += CURRENCY_UNIT[currency];
+      changeSum -= CURRENCY_UNIT[currency];
+      currAmount -= CURRENCY_UNIT[currency];
+      //console.log("amount: ", amount, " changeSum: ", changeSum, " currAmount: ", currAmount)
     }
+    //console.log(amount)
+    if(amount !== 0){
+      change.push([currency, amount/100])
+    }
+    status = "OPEN";
+  }
+  if(changeSum > 0){
+    return {"status": "INSUFFICIENT_FUNDS", "change": []}
+  }
+  if(changeSum == 0 && totalChange == drawerTotal){
+    return {"status": "CLOSED", "change": cid};
   }
 
   return {"status": status, "change": change};
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
